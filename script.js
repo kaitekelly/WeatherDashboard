49434fbd5b4bce2eb18dfda2193e2416286090c7
Kaite Kelly <murphy.kaite@gmail.com(opens in new tab)>
2020-05-01 23:16:04 -0700

    added UV Index API call and weather icon

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let city = $(".city");
 let wind = $(".wind");
 let humidity = $(".humidity");
@@ -15,7 +14,7 @@ let APIKey = "&appid=b9330bfd0b1bf5fe0c849c27df315565";
 
 $(document).ready(function () {
 
-    $("#searchBtn").click(function(event) {
+    $("#searchBtn").click(function (event) {
         event.preventDefault();
 
         //grab search term from input search field
@@ -24,13 +23,13 @@ $(document).ready(function () {
         console.log(searchTerm);
         //construct the URL
         let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
-         searchTerm + APIKey;
+            searchTerm + APIKey;
 
         //add name of search term to search array 
         searchArr.push(searchTerm);
 
         //add search term to top of list of cities
-        $("<li>").text(searchTerm).prepend(".list-group-item");
+        $("<button>").text(searchTerm).prepend(".list-group-item");
 
         //ajax call for local weather
         $.ajax({
@@ -38,56 +37,88 @@ $(document).ready(function () {
             url: queryURL
         }).then(function (response) {
             $("h1").text(JSON.stringify(response));
-          
             //log queryURL
-        console.log(queryURL);
-        //log the resulting object
-        console.log(response);
+            console.log(queryURL);
+            //log the resulting object
+            console.log(response);
+            // let cityResult  = searchTerm;
+            //transfer content to HTML
+            let cityName = $(".jumbotron").text(searchTerm + " Weather Details");
+            let windData = $("<p>").text("Wind Speed: " + response.wind.speed);
+            let humidityData = $("<p>").text("Humidity: " + response.main.humidity + "%");
+            var iconcode = response.weather[0].icon;
+            var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+            let weatherImg = $("<img>").attr("src", iconurl);
 
-        // let cityResult  = searchTerm;
+            // Convert the temp to fahrenheit
+            let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            let roundedTemp = Math.floor(tempF);
 
-        //transfer content to HTML
-        let cityName = $(".jumbotron").text(searchTerm + " Weather Details");
-        let windData = $("<p>").text("Wind Speed: " + response.wind.speed);
-        let humidityData = $("<p>").text("Humidity: " + response.main.humidity + "%");
-        let weatherImg   = $("<img>").text(response.weather[0].icon);
+            //temp elements added to html
+            let tempData = $("<p>").text("Temp (K): " + response.main.temp + "°");
+            let tempDataF = $("<p>").text("Temp (F): " + roundedTemp + "°");
 
-        console.log(response.weather[0].icon);
+            //append the elements together
+            cityName.append(weatherImg, windData, humidityData, tempData, tempDataF);
 
-        // Convert the temp to fahrenheit
-        let tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        let roundedTemp = Math.floor(tempF);
+            $("container").append(cityName);
+            // Log the data in the console as well
+            console.log("Wind Speed: " + response.wind.speed + "MPH");
+            console.log("Humidity: " + response.main.humidity);
+            console.log("Temperature (F): " + tempF);
 
-        //temp elements added to html
-        let tempData = $("<p>").text("Temp (K): " + response.main.temp + "° $Million Dollars$");
-        let tempDataF = $("<p>").text("Temp (F): " + roundedTemp +"°");
+            //need to add CitySearch text to li or city search name to searched list
+            // $("<li>").prepend(".list-group-item");
+            $("<li>").appendTo(".list-group-item").prepend(searchTerm);
 
-        //append the elements together
-        cityName.append(weatherImg, windData, humidityData, tempData, tempDataF);
+            //ajax call for UV Index
+            let latitude = response.coord.lat;
+            let longitude = response.coord.lon;
+            let uvIndexURL = "http://api.openweathermap.org/data/2.5/uvi?" + APIKey + "&lat=" + latitude + "&lon=" + longitude;
+            $.ajax({
+                type: "GET",
+                url: uvIndexURL,
+            }).then(function (responseUV) {
+                console.log(responseUV);
+                let currentUV = $("<span>").text("UV Index: " + responseUV.value).addClass("uv-index");
+                console.log(responseUV.value);
+                cityName.append(currentUV);
 
-        $("container").append(cityName);
+                if (currentUV >= 0 && currentUV < 3) {
+                    $("#uv-index").addClass("badge-success");
+                } else if (currentUV >= 3 && currentUV < 8) {
+                    $("#uv-index").addClass("badge-warning");
+                } else if (currentUV >= 8) {
+                    $("#uv-index").addClass("badge-danger");
+                }
 
-        // Log the data in the console as well
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Temperature (F): " + tempF);
+            })
 
-        //need to add CitySearch text to li or city search name to searched list
-        // $("<li>").prepend(".list-group-item");
-        $("<li>").appendTo(".list-group-item").prepend(searchTerm);
+            //start 5 day forecast ajax
 
-        });
+            let day5QueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + APIKey;
+            for (i = 0; i < 5; i++) {
+
+                $.ajax({
+                    type: "GET",
+                    url: day5QueryURL,
+                }).then(function (response5Day) {
+                    let fiveDay = $("<h3>").text("Temp: " + JSON.stringify(response5Day.list[i].main.temp));
+                    console.log(response5Day);
+                    let fiveHumidity = $("<h3>").text("Humidity: " + JSON.stringify(response.list[i].main.humidity));
+                    console.log(response5Day.list[i].main.temp);
+                    console.log(response.list[i].main.humidity);
+                })
+            }
+
+
+
+
+
+
+        })
 
     })
-    
-    
-    
-    // .then(function(response) {
-        
-
-   
 
 
-
-
-})
+});
